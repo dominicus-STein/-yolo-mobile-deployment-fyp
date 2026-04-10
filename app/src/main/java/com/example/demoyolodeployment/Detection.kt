c675aa68191bdbd89c91faa31f9148df7994a620
dominicus-STein <styap100@gmail.com>
2026-04-11 02:37:12 +0800

    Delete app directory

--- app/src/main/java/com/example/demoyolodeployment/Detection.kt
+++
@@ -1,8 +0,0 @@
-package com.example.demoyolodeployment
-
-import android.graphics.RectF
-data class Detection(
-    val box: RectF,      // left, top, right, bottom in input-image coords
-    val label: String,   // class name
-    val score: Float     // confidence 0.1
-)
